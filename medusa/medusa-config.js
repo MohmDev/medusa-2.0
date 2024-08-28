566f9bdbb6633732227360273649e8f48da6ad3b
Mohamad <Mohammadr_m@outlook.com>
2024-08-28 12:09:33 +0000

    chore: Auth works with NODE_ENV=development

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -16,15 +16,51 @@ module.exports = defineConfig({
         redisUrl: process.env.CACHE_REDIS_URL,
       },
     },
+    [Modules.WORKFLOW_ENGINE]: {
+      resolve: '@medusajs/workflow-engine-redis',
+      options: {
+        redis: {
+          url: process.env.WE_REDIS_URL,
+        },
+      },
+    },
+    [Modules.AUTH]: {
+      resolve: '@medusajs/auth',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/auth-emailpass',
+            id: 'emailpass',
+            options: {
+              // provider options...
+            },
+          },
+        ],
+      },
+    },
   },
+
+  admin: {
+    path: process.env.ADMIN_PATH || `/dashboard`,
+    backendUrl: process.env.MEDUSA_BACKEND_URL || 'http://localhost:9000',
+  },
+
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: process.env.NODE_ENV !== 'development' ? { ssl: { rejectUnauthorized: false } } : {},
+    redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+      workerMode: process.env.WORKER_MODE || 'shared',
+      storeCors: process.env.STORE_CORS,
+      authCors: process.env.AUTH_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authMethodsPerActor: {
+        user: ['emailpass', 'me'],
+        users: ['emailpass', 'me'],
+        customer: ['emailpass', 'google', 'outlook'],
+      },
     },
   },
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,24 @@
+# Medusa user
+
 npx medusa user --email admin@medusa-test.com --password supersecret
+
+---
+
+# Nixpacks
+
+```bash
+# Download the latest release
+curl -LO https://github.com/railwayapp/nixpacks/releases/download/v1.26.1/nixpacks-v1.26.1-x86_64-unknown-linux-gnu.tar.gz
+
+# Extract the downloaded file
+tar -xzf nixpacks-v1.26.1-x86_64-unknown-linux-gnu.tar.gz
+
+# Move the extracted binary to /usr/local/bin
+sudo mv nixpacks /usr/local/bin/
+
+# Clean up the downloaded file
+rm nixpacks-v1.26.1-x86_64-unknown-linux-gnu.tar.gz
+
+# Generate a new Nixpacks build configuration in toml format
+nixpacks plan --format toml .
+```
