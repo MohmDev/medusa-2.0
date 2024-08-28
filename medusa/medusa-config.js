813533fa2431532d7db22bc57a471af3780805b7
Mohamad <Mohammadr_m@outlook.com>
2024-08-28 00:02:04 +0000

    chore: Add Redis caching and event bus modules and update types

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -1,16 +1,30 @@
-import { loadEnv, defineConfig } from '@medusajs/utils'
+import { loadEnv, defineConfig, Modules } from '@medusajs/utils';
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV, process.cwd());
 
 module.exports = defineConfig({
+  modules: {
+    [Modules.EVENT_BUS]: {
+      resolve: '@medusajs/event-bus-redis',
+      options: {
+        redisUrl: process.env.EVENTS_REDIS_URL,
+      },
+    },
+    [Modules.CACHE]: {
+      resolve: '@medusajs/cache-redis',
+      options: {
+        redisUrl: process.env.CACHE_REDIS_URL,
+      },
+    },
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
+});
